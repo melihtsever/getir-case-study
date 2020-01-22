dea627dcef1bffed360fffc39768dfcd6e9e1555
melih topraksever <melihtopraksever@melihs-MacBook-Air.local>
2020-01-22 16:31:38 +0300

    mongo  url

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(`mongodb://${process.env.Mongo_User}@${process.env.Mongo_Ip}/${process.env.Mongo_Db}`, {
+// mongoose.connect(`mongodb://${process.env.Mongo_User}@${process.env.Mongo_Ip}/${process.env.Mongo_Db}`, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true
+// })
+
+mongoose.connect(`mongodb://dbUser:dbPassword1@ds249623.mlab.com:49623/getir-case-study`, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true
 })
+

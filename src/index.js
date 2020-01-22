d5580bc3bca43800fbca4c44202f10a1ad82ec05
melih topraksever <melihtopraksever@melihs-MacBook-Air.local>
2020-01-22 16:58:52 +0300

    a

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-const app = require('./app')
+//const app = require('./app')
 const port = process.env.NODE_PORT
+
+
+const express = require('express')
+require('./db/mongoose')
+const recordRouter = require('./routers/records')
+const app = express()
+const { handleResponse } = require("./middleware/response");
+
+
+app.use(express.json())
+app.use(recordRouter)
+
+// for response middleware
+app.use((responseHandler, req, res, next) => {
+    handleResponse(responseHandler, res);
+});
+
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)

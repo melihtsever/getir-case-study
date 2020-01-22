35546881fa5e1c264e6532caeecb3b2ff559d40c
melih topraksever <melihtopraksever@melihs-MacBook-Air.local>
2020-01-22 17:40:19 +0300

    final fix

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
-// const express = require('express')
-// require('./db/mongoose')
-// const recordRouter = require('./routers/records')
-// const app = express()
-// const { handleResponse } = require("./middleware/response");
+const express = require('express')
+require('./db/mongoose')
+const recordRouter = require('./routers/records')
+const app = express()
+const { handleResponse } = require("./middleware/response");
 
 
-// app.use(express.json())
-// app.use(recordRouter)
+app.use(express.json())
+app.use(recordRouter)
 
-// // for response middleware
-// app.use((responseHandler, req, res, next) => {
-//     handleResponse(responseHandler, res);
-// });
-
-
+// for response middleware
+app.use((responseHandler, req, res, next) => {
+    handleResponse(responseHandler, res);
+});
 
 
 
-// module.exports = app
+
+
+module.exports = app
--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
-//const app = require('./app')
+const app = require('./app')
 const port = process.env.PORT
-
-
-const express = require('express')
-require('./db/mongoose')
-const recordRouter = require('./routers/records')
-const app = express()
-const { handleResponse } = require("./middleware/response");
-
-
-app.use(express.json())
-app.use(recordRouter)
-
-// for response middleware
-app.use((responseHandler, req, res, next) => {
-    handleResponse(responseHandler, res);
-});
-
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
